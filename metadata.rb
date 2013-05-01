f96792b7047fdabe1ccfb35f64547aecfe50a3bb
mkaminskimichal@gmail.com <mkaminskimichal@gmail.com>
2013-05-01 13:27:29 +0200

    remove dependency to magis shell

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,4 @@
 depends          "java"
-depends		 "magic_shell"
 description      "Installs/Configures mule esb, downloads mule project from repository, build by mvn and deploy on mule according to MULE_HOME"
 license          "Apache License"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
